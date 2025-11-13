71c5accfc7524c87182401fce16a587767cdf32e
jaewoo <202017499@jbnu.ac.kr>
2025-11-13 18:32:23 +0900

    feat:test post 임시

--- src/main/java/hello/squadfit/TestController.java
+++ src/main/java/hello/squadfit/TestController.java
@@ -1,20 +1,43 @@
 package hello.squadfit;
 
+import hello.squadfit.domain.member.service.MemberService;
 import hello.squadfit.security.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class TestController {
 
+    private final MemberService memberService;
+
+    public TestController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @GetMapping("/api/test")
-    public Long testPro(@AuthenticationPrincipal CustomUserDetails userDetails){
+    public Long test1(@AuthenticationPrincipal CustomUserDetails userDetails){
       log.info("id = {}", userDetails.getUserId());
 
       return userDetails.getUserId();
 
     }
+
+    @PostMapping("/api/test")
+    public Long test2(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestBody TestRequest request
+            ){
+        log.info("id = {}", userDetails.getUserId());
+
+        return userDetails.getUserId();
+    }
+
+    record TestRequest(
+      String nickname
+    ){}
 }
